c241d139e2adcdaa6382485ade6cdc1e1c79deb1
Xavier VICENTE <xvicente>
2021-07-09 10:33:34 +0200

    Ajout du controller pour les chiffres, Ajoute d'une vue pour les chiffres.

--- ChiffresEtLettres/ChiffresEtLettres/Models/Chiffres.cs
+++ ChiffresEtLettres/ChiffresEtLettres/Models/Chiffres.cs
@@ -8,6 +8,7 @@ namespace ChiffresEtLettres
     public class Chiffres
     {
         public List<int> listChiffre { get; set; }
+        public int ChiffreACalc { get; set; }
 
         private int[] ChiffreDispo = { 1,2,3,4,5,6,7,8,9,10,25,50,75,100 };
         public Chiffres()
@@ -18,6 +19,7 @@ namespace ChiffresEtLettres
             {
                 listChiffre.Add(ChiffreDispo[rand.Next(0, ChiffreDispo.Length)]);
             }
+            ChiffreACalc = rand.Next(101, 1000);
         }
 
         public bool VérifChiffre(string p_chiffresUser)
