d2dbd2b8cd4f152c2f6eb7eef6b99135db125dc4
adam95140 <adam95140>
2021-07-09 17:17:32 +0200

    Pull Chiffres

---
+++ ChiffresEtLettres/ChiffresEtLettres/Controllers/ChiffresController.cs
@@ -0,0 +1,28 @@
+﻿using ChiffresEtLettres.Properties;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace ChiffresEtLettres.Controllers
+{
+    public class ChiffresController : Controller
+    {
+        // GET: LettresController
+        public ActionResult Index()
+        {
+            Chiffres m_chiffres = new Chiffres();
+
+            int nombreAleatoire = m_chiffres.GetNombreAlea();
+
+            ViewBag.Nombre = nombreAleatoire;
+
+            return View();
+        }
+    }
+}
--- ChiffresEtLettres/ChiffresEtLettres/Models/Chiffres.cs
+++ ChiffresEtLettres/ChiffresEtLettres/Models/Chiffres.cs
@@ -7,5 +7,34 @@ namespace ChiffresEtLettres
 {
     public class Chiffres
     {
+        private int nombreAlea;
+        private int nombreAlea_Un;
+        private int nombreAlea_Deux;
+
+        public Chiffres()
+        {
+            Random rand = new Random();
+            int ChiffresRand = rand.Next(100);
+
+            int ChiffresRand_Un = ChiffresRand / 2;
+            int ChiffresRand_Deux = ChiffresRand / 2;
+
+            nombreAlea = ChiffresRand;
+        }
+
+        public int GetNombreAlea()
+        {
+            return nombreAlea;
+        }
+
+        public int GetNombre_Un()
+        {
+            return nombreAlea_Un;
+        }
+
+        public int GetNombre_Deux()
+        {
+            return nombreAlea_Deux;
+        }
     }
 }
